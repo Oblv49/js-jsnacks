4c5497062a5fe22cabb6cf3648aa5069c657f6ea
DavideMinutoli <noprivacyworld@gmail.com>
2023-07-06 23:33:42 +0200

    layout finish all controll finish

--- bonus/bonus1/js/main.js
+++ bonus/bonus1/js/main.js
@@ -13,32 +13,73 @@ let numeriVincenti = generatereRandomArray(6, 1, 90);
 let numeriUtenteArray = [];
 let trovato;
 
-
 for (let i = 0; i < 6; i++) {
 
   let numeroUtente = parseInt(prompt("Inserisci un numero tra 1 e 90: "));
   if (controllNumberRange(numeroUtente, 1, 90)) {
-    numeriUtenteArray.push(numeroUtente);
+
+    if (!numeriUtenteArray.includes(numeroUtente)) {
+
+      numeriUtenteArray.push(numeroUtente);
+
+    } else {
+      document.getElementById("errorNumbersRipetition").classList.remove("d-none");
+      document.getElementById("numbersRandomContainer").classList.add("d-none");
+      document.getElementById("numbersUserContainer").classList.add("d-none");
+      document.getElementById("cardContainer").classList.add("d-none");
+      console.log("Inserisci solo numeri non ripetuti!")
+      break;
+    }
+
 
   } else {
-    
+
     document.getElementById("errorNumbers").classList.remove("d-none");
+    document.getElementById("numbersRandomContainer").classList.add("d-none");
+    document.getElementById("numbersUserContainer").classList.add("d-none");
     document.getElementById("cardContainer").classList.add("d-none");
     console.log("Inserisci solo numeri tra 1 e 90!")
-    
+
     break;
-  
+
   }
-  
+
   let trovato = searchInArray(numeriVincenti, numeriUtenteArray);
 
 
 
 }
 
+//numeri random dom
+for (let i = 0; i < numeriVincenti.length; i++) {
 
-document.getElementById("numbersRandom").innerHTML ="I Numeri Vincenti sono: " + numeriVincenti;
-document.getElementById("numbersUser").innerHTML = "I tuoi Numeri sono: " + numeriUtenteArray;
+  let color = 'bg-success';
+
+  document.getElementById("numbersRandomContainer").innerHTML += `
+      <div class="square rounded-circle ${color}">
+        <div>
+          <p class="text-center fs-5 fw-3 vertical-align-square shadow">${numeriVincenti[i]}</p>
+        </div>
+      </div>
+  `;
+
+}
+
+//numeri utente dom
+for (let i = 0; i < numeriUtenteArray.length; i++) {
+
+  let color = 'bg-light';
+
+  document.getElementById("numbersUserContainer").innerHTML += `
+      <div class="square mb-5 rounded-circle ${color}">
+        <div>
+          <p class="text-center fs-5 fw-3 vertical-align-square shadow">${numeriUtenteArray[i]}</p>
+        </div>
+      </div>
+  `;
+
+}
+
 
 
 console.log("numeri random " + numeriVincenti);
